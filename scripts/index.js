ec4d606eb572dd75fecbd8c932049fda267ac1ef
guy20056 <guy20056@walla.com>
2019-06-05 15:56:58 +0300

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -150,19 +150,19 @@ $(function() {
     setTimeout(function() { onScrollInit($('.waypoint')) }, 10);
   
     // CONTACT FORM
-    $('#contact-form').submit(function(e) {
-      e.preventDefault();
+    // $('#contact-form').submit(function(e) {
+    //   e.preventDefault();
   
-        $.ajax({
-            url: "https://formspree.io/guyg20056@gmail.com",
-            method: "POST",
-            data: { message: $('form').serialize() },
-            dataType: "json"
-        }).done(function(response) {
-            $('#success').addClass('expand');
-            $('#contact-form').find("input[type=text], input[type=email], textarea").val("");
-        });
-    });
+    //     $.ajax({
+    //         url: "https://formspree.io/guyg20056@gmail.com",
+    //         method: "POST",
+    //         data: { message: $('form').serialize() },
+    //         dataType: "json"
+    //     }).done(function(response) {
+    //         $('#success').addClass('expand');
+    //         $('#contact-form').find("input[type=text], input[type=email], textarea").val("");
+    //     });
+    // });
   
     $('#close').click(function() {
       $('#success').removeClass('expand');
